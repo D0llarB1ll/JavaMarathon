c620b0016d5905d84b6a44b0c985e18c9a93f279
BILL STERN <91071760+D0llarB1ll@users.noreply.github.com>
2021-10-11 08:29:06 +0300

    Create Task3.java

---
+++ Day 3/Task3.java
@@ -0,0 +1,21 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int i = 0;
+        while (i < 5){
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            i++;
+            if (divider == 0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(dividend / divider);
+
+        }
+    }
+}
